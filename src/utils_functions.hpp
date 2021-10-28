2c14d8b0e0e33944704fdbcd5ee4be1dcd67279b
Juan Pablo Castillo <juan.castillog@sansano.usm.cl>
2021-10-28 04:04:52 -0300

    Update utils_functions.hpp

--- src/utils_functions.hpp
+++ src/utils_functions.hpp
@@ -5,7 +5,7 @@
 #include "flatBinTrie.hpp"
 
 void decodeBinTrie(binTrie b, vector<uint64_t> &decoded, bit_vector partial_result, uint64_t node_id, uint16_t curr_level, uint16_t max_level);
-void decodeBinTrie(flatBinTrie b, vector<uint64_t> &decoded, bit_vector partial_result, uint64_t node_id, uint16_t curr_level, uint16_t max_level);
+void decodeBinTrie(flatBinTrie &b, vector<uint64_t> &decoded, bit_vector partial_result, uint64_t node_id, uint16_t curr_level, uint16_t max_level);
 vector<uint64_t>* read_inverted_list(std::ifstream &input_stream, uint64_t n);
 void read_inverted_index(string file_path);
 
