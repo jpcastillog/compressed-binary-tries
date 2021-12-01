f281d09b9dfb43c6957b3b903dcf3cbacb8ab152
Juan Pablo Castillo <juan.castillog@sansano.usm.cl>
2021-11-30 23:45:40 -0300

    add compress intersection

--- src/intersection.cpp
+++ src/intersection.cpp
@@ -284,3 +284,88 @@ flatBinTrie<rankType> intersectFlatTries(vector<flatBinTrie<rankType>> &Bs) {
 }
 template flatBinTrie<rank_support_v5<1>> intersectFlatTries<rank_support_v5<1>>(vector<flatBinTrie<rank_support_v5<1>>> &Bs);
 template flatBinTrie<rank_support_v<1>> intersectFlatTries<rank_support_v<1>>(vector<flatBinTrie<rank_support_v<1>>> &Bs);
+
+
+template <class rankType>
+void compressedIntersection(vector <flatBinTrie<rankType>> &Bs, uint16_t max_level, uint16_t curr_level, 
+                vector<uint64_t> roots, vector<uint64_t> &last_pos,
+                vector<uint64_t> ones_to_write[], vector<uint64_t> &nodes_per_level) {
+
+    // End condition
+	if (curr_level == max_level - 1) {
+        uint16_t n_tries = Bs.size();
+        bit_vector result = bit_vector(2, 1); // vector de 2 bits con 1's
+        // Compute "bitwise and" between nodes of n_tries  
+        for (uint16_t i = 0; i < n_tries; ++i) {
+            bit_vector node_i = Bs[i].getNode(roots[i]);
+            result &= node_i; 
+        }
+        // Only write when result = 11, 10 or 01
+        if (result[0] || result[1]){
+            if (result[0])
+                ones_to_write[curr_level].push_back(last_pos[curr_level]);
+            if (result[1])
+                ones_to_write[curr_level].push_back(last_pos[curr_level] + 1);
+            last_pos[curr_level] += 2;
+        }
+		return;
+	}
+	
+	uint16_t n_tries = Bs.size();
+	bit_vector result = bit_vector(2, 1); // vector de 2 bits con 1's
+    // Compute "bitwise and" between nodes of n_tries  
+	for (uint16_t i = 0; i < n_tries; ++i) {
+		bit_vector node_i = Bs[i].getNode(roots[i]);
+		result &= node_i; 
+	}
+    // cout << result << endl;
+
+	uint16_t next_level = curr_level + 1;
+    uint64_t pos_next_level_before = last_pos[next_level];;
+
+    bool exist_lchild;
+    bool exist_rchild;
+	// Left child
+    vector <uint64_t> left_nodes;
+	if (result[0] == 1) {
+		for (uint64_t i = 0; i < n_tries; ++i) {
+			left_nodes.push_back(Bs[i].getLeftChild(roots[i]));
+		}
+		intersectionV2(Bs, max_level, next_level, left_nodes, last_pos, ones_to_write, nodes_per_level);
+	}
+
+    if (last_pos[next_level] == pos_next_level_before) {
+        exist_lchild = false;
+    }
+    else  {
+        ones_to_write[curr_level].push_back(last_pos[curr_level]);
+        pos_next_level_before = last_pos[next_level];
+        exist_lchild = true;
+    }
+	// Right child
+    vector <uint64_t> right_nodes;
+	if (result[1] == 1) {
+		for (uint64_t i = 0; i < n_tries; ++i) {
+            if (left_nodes.size() > 0) {
+                right_nodes.push_back(left_nodes[i] + 1);
+            }
+            else {
+                right_nodes.push_back(Bs[i].getRightChild(roots[i]));
+            }
+			
+		}
+		intersectionV2(Bs, max_level, next_level, right_nodes, last_pos, ones_to_write, nodes_per_level);
+	}
+    
+    if (last_pos[next_level] == pos_next_level_before) {
+        exist_rchild = false;
+    }
+    else {
+        ones_to_write[curr_level].push_back(last_pos[curr_level] + 1);
+        exist_rchild = true;
+    }
+    // mark if node is write
+    if (exist_lchild || exist_rchild) {
+        last_pos[curr_level] += 2;
+    }    
+}
