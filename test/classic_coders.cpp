53e53af0c2304727851360ab4dafc54ea24515d1
Juan Pablo Castillo <juan.castillog@sansano.usm.cl>
2022-06-18 02:11:24 -0400

    add classic coders

---
+++ test/classic_coders.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sdsl/vectors.hpp>
+
+using namespace std;
+// Compile
+// g++ -std=c++11 -O3 -DNDEBUG -I ~/include -L ~/lib classic_coders.cpp -o classic_coders.out -lsdsl -ldivsufsort -ldivsufsort64
+
+//  SDSL Coders
+// sdsl::coder::elias_delta
+// sdsl::coder::elias_gamma
+// sdsl::coder::fibonacci
+template<class Coder>
+sdsl::enc_vector<Coder>* classicEncode(std::vector<uint64_t> &v) {
+    sdsl::int_vector<> int_v(v.size());
+    
+    std::vector<uint64_t>::iterator it;
+    uint64_t i = 0;
+    for (it = v.begin(); it != v.end(); it++) {
+        int_v[i] = *it;
+        ++i;
+    }
+    sdsl::util::bit_compress(int_v);
+    sdsl::enc_vector<Coder>*ev;
+    ev = new sdsl::enc_vector<Coder>(int_v);
+    return ev;
+}
+
+int main(int argc, const char** argv) {
+    std::string collection = "";
+    uint64_t min_length = 0;
+    int mandatory = 2;
+
+    if (argc < mandatory){
+        std::cout   << "collection filename"
+                        "[--min_length l]"
+                    <<
+        std::endl;
+        return 1;
+    } 
+
+    collection = std::string(argv[1]);
+    for (int i = 0; i < argc; ++i) {
+        if(std::string(argv[i]) == "--min_length"){
+            ++i;
+            min_length = std::stoull(argv[i]);
+        }
+    }
+
+    std::ifstream in;
+    in.open(collection, std::ios::binary | std::ios::in);
+    if (!in.is_open()) {
+        std::cout << "Can't open file " << collection << std::endl;
+    }
+
+    // uint64_t gap = 0, rle = 0, trie = 0, trie_run = 0; 
+    uint64_t elias_delta = 0, elias_gamma = 0, fibonacci = 0;
+    uint64_t nElements = 0, nSets = 0;
+    uint64_t n_il = 0;
+    uint32_t n, x;
+    uint32_t u, _1;
+    in.read(reinterpret_cast<char*>(&u), 4);
+    in.read(reinterpret_cast<char*>(&_1), 4);
+    while (true) {
+        if (in.eof()) {
+            break;
+        }
+        in.read(reinterpret_cast<char*>(&n), 4);
+        if (n > min_length){
+            nElements += n;
+            nSets++;
+            std::vector<uint64_t> s;
+            s.reserve(n);
+            for (uint64_t i = 0; i < n; ++i) {
+                in.read(reinterpret_cast<char*>(&x), 4);
+                s.push_back((uint64_t)x);
+            }
+            // Encode sets
+            sdsl::enc_vector<sdsl::coder::elias_delta>* ed;
+            sdsl::enc_vector<sdsl::coder::elias_gamma>* eg;
+            sdsl::enc_vector<sdsl::coder::fibonacci>* fib;
+            
+            ed  = classicEncode<sdsl::coder::elias_delta>(s);
+            eg  = classicEncode<sdsl::coder::elias_gamma>(s);
+            fib = classicEncode<sdsl::coder::fibonacci>(s);
+            // Size in bytes
+            elias_delta += sdsl::size_in_bytes(*ed);
+            elias_gamma += sdsl::size_in_bytes(*eg);
+            fibonacci   += sdsl::size_in_bytes(*fib);
+            n_il++;
+
+            delete ed;
+            delete eg;
+            delete fib;
+            
+        } else {
+            in.seekg(4*n, std::ios::cur);
+        }
+        if (n_il %1000 == 0)
+            std::cout << n_il << " sets processed" << std::endl;
+    }
+    in.close();
+    std::cout << "#Ints: " << nElements << std::endl;
+    std::cout << "#Sets: " << nSets << std::endl;
+    std::cout << "Avg Elias Delta: " << (double)elias_delta*8/nElements << " bpi" << std::endl;
+    std::cout << "Avg Elias Gamma: " << (double)elias_gamma*8/nElements << " bpi" << std::endl;
+    std::cout << "Avg Fibonacci: " << (double)fibonacci*8/nElements << " bpi" << std::endl;
+    // std::cout << "Trie run: " << (double)trie_run/nElements << "bpi" << std::endl;
+    return 0;
+}
--- test/measures.cpp
+++ test/measures.cpp
@@ -17,50 +17,75 @@ uint64_t Gap(std::vector<uint64_t> &v) {
     uint64_t x_i1, x_i, g_i;
     
     std::vector<uint64_t>::iterator it;
-    x_i1 = *(v.begin());
-    for (it = v.begin() + 1; it != v.end(); it++) {
+    x_i1 = v[0];
+    it = v.begin();
+    it++;
+    for (; it != v.end(); it++) {
         x_i = *it;
         g_i = x_i - x_i1 - 1;
         if (g_i == 0)
             g_i = 1;
-        Gap += floor(g_i) + 1;
+        Gap += floor(log2(g_i)) + 1;
+        x_i1 = x_i;
     }
     return Gap;
 }
 
 uint64_t Rle(std::vector<uint64_t> &v) {
     uint64_t rle = 0;
-    uint64_t u = *(v.end());
+    // uint64_t u = *v.end();
+    // uint64_t u = v[v.size()-1];
     // First create cbv
-    sdsl::bit_vector cbv(0, u+1); //characteristic bit vector (cbv)
+    // sdsl::bit_vector cbv(u+1, 0); //characteristic bit vector (cbv)
     std::vector<uint64_t>::iterator it;
-    for(it = v.begin()+1; it != v.end(); it++)
-        cbv[*it] = 1;
+    // for(it = v.begin()+1; it != v.end(); it++)
+    //     cbv[*it] = 1;
     // End create cbv
     uint64_t x, x_i;
     uint64_t runLength = 1;
-    x = cbv[1];
-    for(uint64_t i = 1; i < cbv.size(); ++i) {
-        x_i = cbv[i];
-        if (x_i != x) {
-            uint64_t sub = runLength - 1;
-            if (sub == 0)
-                sub = 1;
-            rle += floor(log(sub)) + 1;
+    it = v.begin();
+    x = *it;
+    it++;
+    for (; it != v.end(); it++){\
+        x_i = *it;
+        uint64_t delta = x_i - x;
+        uint64_t sub0, sub1;
+        if (delta == 1){
+            runLength++;
+        } else {
+            sub0 = (delta - 1) - 1;
+            sub1 = runLength - 1;
+            if (sub0 == 0) sub0 = 1;
+            if (sub1 == 0) sub1 = 1;
+            rle += floor(log2(sub0)) + 1;
+            rle += floor(log2(sub1)) + 1;
             runLength = 1;
-        }
-        else {
-            ++runLength;
         }
         x = x_i;
     }
+    // x = cbv[1];
+    // for(uint64_t i = 1; i < cbv.size(); ++i) {
+    //     x_i = cbv[i];
+    //     if (x_i != x) {
+    //         uint64_t sub = runLength - 1;
+    //         if (sub == 0)
+    //             sub = 1;
+    //         rle += floor(log2(sub)) + 1;
+    //         runLength = 1;
+    //     }
+    //     else {
+    //         ++runLength;
+    //     }
+    //     x = x_i;
+    // }
 
     return rle;
 }
 
 // if encodeRuns is true the measure is trie-run
 uint64_t Trie(std::vector<uint64_t> &v, bool encodeRuns) {
-    uint64_t u = *(v.end());
+    // uint64_t u = *v.end();
+    uint64_t u = v[v.size() - 1];
     flatBinTrie<rank_support_v<1>> t = flatBinTrie<rank_support_v<1>>(v, u);
     if (encodeRuns)
         t.encodeRuns();
@@ -99,6 +124,7 @@ int main(int argc, const char** argv) {
 
     uint64_t gap = 0, rle = 0, trie = 0, trie_run = 0; 
     uint64_t nElements = 0, nSets = 0;
+    uint64_t n_il = 0;
     uint32_t n, x;
     uint32_t u, _1;
     in.read(reinterpret_cast<char*>(&u), 4);
@@ -111,30 +137,33 @@ int main(int argc, const char** argv) {
         if (n > min_length){
             nElements += n;
             nSets++;
-            std::vector<uint64_t> *s = new std::vector<uint64_t>();
-            // s -> reserve(n);
+            std::vector<uint64_t> s;
+            s.reserve(n);
             for (uint64_t i = 0; i < n; ++i) {
                 in.read(reinterpret_cast<char*>(&x), 4);
-                s -> push_back((uint64_t)x);
+                s.push_back((uint64_t)x);
             }
             // Calculate Measures
-            gap += Gap(*s);
-            rle += Rle(*s);
-            trie += Trie(*s, false);
-            trie_run += Trie(*s, true);
-            delete s;
+            gap += Gap(s);
+            rle += Rle(s);
+            // trie += Trie(s, false);
+            // trie_run += Trie(s, true);
+            // delete s;
+            n_il++;
             
         } else {
             in.seekg(4*n, std::ios::cur);
         }
+        if (n_il %1000 == 0)
+            std::cout << n_il << " sets processed" << std::endl;
     }
     in.close();
     std::cout << "#Ints: " << nElements << std::endl;
     std::cout << "#Sets: " << nSets << std::endl;
     std::cout << "Avg Gap: " << (double)gap/nElements << "bpi" << std::endl;
     std::cout << "Avg RLE: " << (double)rle/nElements << "bpi" << std::endl;
-    std::cout << "Avg Trie: " << (double)trie/nElements << "bpi" << std::endl;
-    std::cout << "Trie run: " << (double)trie_run/nElements << "bpi" << std::endl;
+    // std::cout << "Avg Trie: " << (double)trie/nElements << "bpi" << std::endl;
+    // std::cout << "Trie run: " << (double)trie_run/nElements << "bpi" << std::endl;
 
     return 0;
 }
